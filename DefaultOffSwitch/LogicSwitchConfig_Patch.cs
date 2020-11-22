5466f36337ac847bf5fde89fb25251632f3b7288
PsychoDad <52836134+PsychoDad9999@users.noreply.github.com>
2020-11-22 16:04:28 +0100

    Adding Mod Default Off Switch

---
+++ DefaultOffSwitch/LogicSwitchConfig_Patch.cs
@@ -0,0 +1,23 @@
+﻿// ----------------------------------------------------------------------------
+
+using Harmony;
+using UnityEngine;
+
+// ----------------------------------------------------------------------------
+
+namespace OniMods.DefaultOffSwitch
+{
+    [HarmonyPatch(typeof(LogicSwitchConfig))]
+    [HarmonyPatch(nameof(LogicSwitchConfig.DoPostConfigureComplete))]
+    static class LogicSwitchConfig_DoPostConfigureComplete_Patch
+    {
+        /// <summary>
+        /// LogicSwitchConfig.DoPostConfigureComplete Postfix
+        /// </summary>
+        static void Postfix(GameObject go)
+        {
+            // Set default state
+            go.AddOrGet<LogicSwitch>().defaultState = false;           
+        }
+    }
+}
