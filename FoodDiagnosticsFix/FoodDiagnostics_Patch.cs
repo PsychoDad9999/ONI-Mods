a14f7ba8a4e2148754ea3c4b4e519b3bd0e25d7f
PsychoDad <52836134+PsychoDad9999@users.noreply.github.com>
2022-02-27 11:01:37 +0100

    Minor improvments

--- FoodDiagnosticsFix/FoodDiagnostics_Patch.cs
+++ FoodDiagnosticsFix/FoodDiagnostics_Patch.cs
@@ -19,10 +19,9 @@ namespace OniMods.FoodDiagnosticsFix
         /// <summary>
         /// FoodDiagnostic.CheckEnoughFood Prefix
         /// </summary>        
-        static bool Prefix(FoodDiagnostic __instance, ref ColonyDiagnostic.DiagnosticResult __result)
+        static bool Prefix(FoodDiagnostic __instance, ref ColonyDiagnostic.DiagnosticResult __result, float ___trackerSampleCountSeconds)
         {            
             __result = new ColonyDiagnostic.DiagnosticResult(ColonyDiagnostic.DiagnosticResult.Opinion.Normal, UI.COLONY_DIAGNOSTICS.GENERIC_CRITERIA_PASS);
-           
 
             if (__instance.tracker.GetDataTimeLength() < 10f)
             {
@@ -32,12 +31,11 @@ namespace OniMods.FoodDiagnosticsFix
             else
             {
                 List<MinionIdentity> dupes = Components.LiveMinionIdentities.GetWorldItems(__instance.worldID);
-                float trackerSampleCountSeconds = Traverse.Create<FoodDiagnostic>().Field("trackerSampleCountSeconds").GetValue<float>();
                 float requiredCaloriesPerCycle = GetRequiredFoodPerCycleByAttributeModifier(dupes);
 
                 int daysReserve = 3; // show warning if food doesn't last for 3 days
 
-                if (requiredCaloriesPerCycle * daysReserve > __instance.tracker.GetAverageValue(trackerSampleCountSeconds))
+                if (requiredCaloriesPerCycle * daysReserve > __instance.tracker.GetAverageValue(___trackerSampleCountSeconds))
                 {
                     __result.opinion = ColonyDiagnostic.DiagnosticResult.Opinion.Concern;
                     float currentValue = __instance.tracker.GetCurrentValue();
@@ -67,13 +65,13 @@ namespace OniMods.FoodDiagnosticsFix
         /// <returns>Returns calories per cycle</returns>
         private static float GetRequiredFoodPerCycleByAttributeModifier(List<MinionIdentity> dupes)
         {
-            float totalCalories = 0;
+            float totalCalories = 0f;
 
             if (dupes != null)
             {               
                 foreach (MinionIdentity dupe in dupes)
                 {
-                    float caloriesPerSecond = dupe.GetAmounts().Get(Db.Get().Amounts.Calories).GetDelta();
+                    float caloriesPerSecond = Db.Get().Amounts.Calories.Lookup(dupe).GetDelta();
 
                     // "tummyless" attribute adds float.PositiveInfinity
                     if (caloriesPerSecond != float.PositiveInfinity)
